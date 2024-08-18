6a7c7959f0631ea6772cb83496305cf1e0e890f3
EMMANUEL <50409804+emmy-1@users.noreply.github.com>
2024-08-18 13:52:50 +0100

    update to database

--- Database/dev/Database connection.py
+++ Database/dev/Database connection.py
@@ -1,2 +1,7 @@
 # Databricks notebook source
+import sqlite3
+import os
+os.chdir("https://github.com/emmy-1/subscriber_cancellations/blob/main/Database/dev/cademycode_updated.db")
 
+con = sqlite3.connect("cademycode.db")
+cur = con.cursor()
